6bca521865ce7be82355a0d48522cbbef7a8f7eb
carlos.bahena <carlos.bahena@wssgroup.com>
2020-12-03 08:35:49 -0600

    Commit de Inicio

---
+++ AppDemo/Views/AboutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDemo.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        public void GetData(object sender, EventArgs e)
+        {
+            var url = urlEntry.Text; //"https://" + 
+            Browser.Source = url;
+        }
+    }
+}
